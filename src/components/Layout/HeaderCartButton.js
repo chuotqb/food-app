eeac33c6d02d10c1ad574ad3c9c1bd89f9017f5c
Thành Đạt <thanhdat.dut@gmail.com>
2022-08-29 23:00:40 +0700

    up

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,46 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from './HeaderCartButton.module.css';
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const { items } = cartCtx;
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnHighlighted(false);
+        }, 300)
+
+        return () => { 
+            clearTimeout(timer);
+        };
+    }, [cartCtx])
+
+    return (
+        <button className={btnClasses} onClick={props.onClick}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>
+                {numberOfCartItems}
+            </span>
+        </button>
+    )
+};
+
+export default HeaderCartButton;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSumary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+    return <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+};
+
+export default Meals;
